ed9272b6b885b499b562a335b7125b73bf5636b8
Sergey Nedobezhkin <zelenka1633@gmail.com>
2023-04-08 17:03:13 +0400

    Вынес баннер меню в отдельный файл компонент

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -2,13 +2,13 @@ import React from "react";
 import styled from "styled-components";
 import MenuBannerImg from "../image/banner.png";
 
-const MenuBannerStyleImg = styled.img`
+const MenuBannerStyleImg = styled.div`
   width: 100%;
-  height: 100%;
+  height: 300px;
+  padding-top: 77px;
+  background-image: url(${MenuBannerImg});
   background-position: center;
   background-size: cover;
 `;
 
-export const Banner = (pros) => (
-  <MenuBannerStyleImg src={MenuBannerImg} alt="Banner-burger" />
-);
+export const Banner = (pros) => <MenuBannerStyleImg />;
