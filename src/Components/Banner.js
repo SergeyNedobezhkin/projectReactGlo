ff70beb6ef640532c95670b385003dac068028d7
Sergey Nedobezhkin <zelenka1633@gmail.com>
2023-04-08 16:38:45 +0400

    Вынес баннер меню в отдельный файл компонент

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -5,6 +5,8 @@ import MenuBannerImg from "../image/banner.png";
 const MenuBannerStyleImg = styled.img`
   width: 100%;
   height: 100%;
+  background-position: center;
+  background-size: cover;
 `;
 
 export const Banner = (pros) => (
